7e6ac06e31cdb186a9655c793a9e166d414786e7
Ian Hays <ianha@microsoft.com>
2017-08-28 10:40:09 -0700

    Expose ECDiffieHellman in Algorithms - netcoreapp only

--- src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/ECDiffieHellmanCng.cs
+++ src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/ECDiffieHellmanCng.cs
@@ -8,41 +8,6 @@ using static Internal.NativeCrypto.BCryptNative;
 
 namespace System.Security.Cryptography
 {
-    public abstract partial class ECDiffieHellman : AsymmetricAlgorithm
-    {
-        /// <summary>
-        /// Creates an instance of the platform specific implementation of the cref="ECDiffieHellman" algorithm.
-        /// </summary>
-        public static new ECDiffieHellman Create()
-        {
-            return new ECDiffieHellmanImplementation.ECDiffieHellmanCng();
-        }
-
-        /// <summary>
-        /// Creates a new instance of the default implementation of the Elliptic Curve Diffie-Hellman Algorithm
-        /// (ECDH) with a newly generated key over the specified curve.
-        /// </summary>
-        /// <param name="curve">The curve to use for key generation.</param>
-        /// <returns>A new instance of the default implementation of this class.</returns>
-        public static ECDiffieHellman Create(ECCurve curve)
-        {
-            return new ECDiffieHellmanImplementation.ECDiffieHellmanCng(curve);
-        }
-
-        /// <summary>
-        /// Creates a new instance of the default implementation of the Elliptic Curve Diffie-Hellman Algorithm
-        /// (ECDH) using the specified ECParameters as the key.
-        /// </summary>
-        /// <param name="parameters">The parameters representing the key to use.</param>
-        /// <returns>A new instance of the default implementation of this class.</returns>
-        public static ECDiffieHellman Create(ECParameters parameters)
-        {
-            ECDiffieHellman ecdh = new ECDiffieHellmanImplementation.ECDiffieHellmanCng();
-            ecdh.ImportParameters(parameters);
-            return ecdh;
-        }
-    }
-
     internal static partial class ECDiffieHellmanImplementation
     {
         public sealed partial class ECDiffieHellmanCng : ECDiffieHellman
--- src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/ECDiffieHellmanPublicKey.cs
+++ src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/ECDiffieHellmanPublicKey.cs
@@ -7,7 +7,7 @@ namespace System.Security.Cryptography
     /// <summary>
     ///     Wrapper for public key material passed between parties during Diffie-Hellman key material generation
     /// </summary>
-    public abstract class ECDiffieHellmanPublicKey : IDisposable
+    public abstract partial class ECDiffieHellmanPublicKey : IDisposable
     {
         private readonly byte[] _keyBlob;
 
@@ -37,26 +37,6 @@ namespace System.Security.Cryptography
         public virtual string ToXmlString()
         {
             throw new NotImplementedException(SR.NotSupported_SubclassOverride);
-        }
-
-        /// <summary>
-        /// When overridden in a derived class, exports the named or explicit ECParameters for an ECCurve.
-        /// If the curve has a name, the Curve property will contain named curve parameters, otherwise it
-        /// will contain explicit parameters.
-        /// </summary>
-        /// <returns>The ECParameters representing the point on the curve for this key.</returns>
-        public virtual ECParameters ExportParameters()
-        {
-            throw new NotSupportedException(SR.NotSupported_SubclassOverride);
-        }
-
-        /// <summary>
-        /// When overridden in a derived class, exports the explicit ECParameters for an ECCurve.
-        /// </summary>
-        /// <returns>The ECParameters representing the point on the curve for this key, using the explicit curve format.</returns>
-        public virtual ECParameters ExportExplicitParameters()
-        {
-            throw new NotSupportedException(SR.NotSupported_SubclassOverride);
         }
     }
 }
