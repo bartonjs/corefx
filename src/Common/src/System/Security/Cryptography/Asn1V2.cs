8d89537ebec9efa9101f24eef24ceba346b98f75
Jeremy Barton <jbarton@microsoft.com>
2017-09-01 16:19:38 -0700

    ReadBMPString

--- src/Common/src/System/Security/Cryptography/Asn1V2.cs
+++ src/Common/src/System/Security/Cryptography/Asn1V2.cs
@@ -231,6 +231,7 @@ namespace System.Security.Cryptography.Asn1
         private const int EndOfContentsEncodedLength = 2;
 
         private static readonly Text.Encoding s_utf8Encoding = new UTF8Encoding(false, true);
+        private static readonly Text.Encoding s_bmpEncoding = new BMPEncoding();
 
         private ReadOnlySpan<byte> _data;
 
@@ -1796,6 +1797,55 @@ namespace System.Security.Cryptography.Asn1
             return new AsnReader(contents);
         }
 
+        /// <summary>
+        /// Gets the source data for a BMPString under a primitive encoding.
+        /// </summary>
+        /// <param name="ruleSet">The encoding rules for the reader.</param>
+        /// <param name="contents">The content bytes for the BMPString payload.</param>
+        /// <returns>
+        ///   <c>true</c> if the octet string uses a primitive encoding, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="CryptographicException">
+        ///  <ul>
+        ///   <li>No data remains</li>
+        ///   <li>The tag is invalid for a BMPString value</li>
+        ///   <li>The length is invalid under the chosen encoding rules</li>
+        ///   <li>A CER encoding was chosen and the primitive content length exceeds the maximum allowed</li>
+        /// </ul>
+        /// </exception>
+        public bool TryGetBMPStringBytes(AsnEncodingRules ruleSet, out ReadOnlySpan<byte> contents)
+        {
+            return TryGetOctetStringBytes(ruleSet, UniversalTagNumber.BMPString, out contents);
+        }
+
+        public bool TryCopyBMPStringBytes(AsnEncodingRules ruleSet, Span<byte> destination, out int bytesWritten)
+        {
+            bool copied = TryCopyCharacterStringBytes(
+                ruleSet,
+                UniversalTagNumber.BMPString,
+                destination,
+                true,
+                out int bytesRead,
+                out bytesWritten);
+
+            if (copied)
+            {
+                _data = _data.Slice(bytesRead);
+            }
+
+            return copied;
+        }
+
+        public bool TryCopyBMPString(AsnEncodingRules ruleSet, Span<char> destination, out int charsWritten)
+        {
+            return TryCopyCharacterString(
+                ruleSet,
+                UniversalTagNumber.BMPString,
+                s_bmpEncoding,
+                destination,
+                out charsWritten);
+        }
+
         private static ReadOnlySpan<byte> Slice(ReadOnlySpan<byte> source, int offset, int length)
         {
             Debug.Assert(offset >= 0);
@@ -1837,6 +1887,178 @@ namespace System.Security.Cryptography.Asn1
             {
                 throw new CryptographicException(SR.Cryptography_Der_Invalid_Encoding);
             }
+        }
+    }
+
+    internal abstract class SpanBasedEncoding : Text.Encoding
+    {
+        protected SpanBasedEncoding()
+            : base(0, EncoderFallback.ExceptionFallback, DecoderFallback.ExceptionFallback)
+        {
+        }
+
+        protected abstract int GetBytes(ReadOnlySpan<char> chars, Span<byte> bytes, bool write);
+        protected abstract int GetChars(ReadOnlySpan<byte> bytes, Span<char> chars, bool write);
+
+        public override int GetByteCount(char[] chars, int index, int count)
+        {
+            return GetByteCount(new ReadOnlySpan<char>(chars, index, count));
+        }
+
+        public override unsafe int GetByteCount(char* chars, int count)
+        {
+            return GetByteCount(new ReadOnlySpan<char>(chars, count));
+        }
+
+        public override int GetByteCount(string s)
+        {
+            return GetByteCount(s.AsReadOnlySpan());
+        }
+
+        private int GetByteCount(ReadOnlySpan<char> chars)
+        {
+            return GetBytes(chars, Span<byte>.Empty, write: false);
+        }
+        
+        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+        {
+            return GetBytes(
+                new ReadOnlySpan<char>(chars, charIndex, charCount),
+                new Span<byte>(bytes, byteIndex, bytes.Length - byteIndex),
+                write: true);
+        }
+
+        public override unsafe int GetBytes(char* chars, int charCount, byte* bytes, int byteCount)
+        {
+            return GetBytes(
+                new ReadOnlySpan<char>(chars, charCount),
+                new Span<byte>(bytes, byteCount),
+                write: true);
+        }
+
+        public override int GetCharCount(byte[] bytes, int index, int count)
+        {
+            return GetCharCount(new ReadOnlySpan<byte>(bytes, index, count));
+        }
+
+        public override unsafe int GetCharCount(byte* bytes, int count)
+        {
+            return GetCharCount(new ReadOnlySpan<byte>(bytes, count));
+        }
+
+        private int GetCharCount(ReadOnlySpan<byte> bytes)
+        {
+            return GetChars(bytes, Span<char>.Empty, write: false);
+        }
+
+        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
+        {
+            return GetChars(
+                new ReadOnlySpan<byte>(bytes, byteIndex, byteCount),
+                new Span<char>(chars, charIndex, chars.Length - charIndex),
+                write: true);
+        }
+
+        public override unsafe int GetChars(byte* bytes, int byteCount, char* chars, int charCount)
+        {
+            return GetChars(
+                new ReadOnlySpan<byte>(bytes, byteCount),
+                new Span<char>(chars, charCount),
+                write: true);
+        }
+    }
+
+    /// <summary>
+    /// Big-Endian UCS-2 encoding (the same as UTF-16BE, but disallowing surrogate pairs to leave plane 0)
+    /// </summary>
+    internal class BMPEncoding : SpanBasedEncoding
+    {
+        protected override int GetBytes(ReadOnlySpan<char> chars, Span<byte> bytes, bool write)
+        {
+            if (chars.IsEmpty)
+                return 0;
+
+            int writeIdx = 0;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char c = chars[i];
+
+                if (char.IsSurrogate(c))
+                {
+                    EncoderFallback.CreateFallbackBuffer().Fallback(c, i);
+                }
+
+                ushort val16 = c;
+
+                if (write)
+                {
+                    bytes[writeIdx + 1] = (byte)val16;
+                    bytes[writeIdx] = (byte)(val16 >> 8);
+                }
+
+                writeIdx += 2;
+            }
+
+            return writeIdx;
+        }
+
+        protected override int GetChars(ReadOnlySpan<byte> bytes, Span<char> chars, bool write)
+        {
+            if (bytes.IsEmpty)
+            {
+                return 0;
+            }
+
+            if (bytes.Length % 2 != 0)
+            {
+                DecoderFallback.CreateFallbackBuffer().Fallback(
+                    bytes.Slice(bytes.Length - 1).ToArray(),
+                    bytes.Length - 1);
+
+                Debug.Fail("Fallback should have thrown");
+                throw new CryptographicException();
+            }
+
+            int writeIdx = 0;
+
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                int val = bytes[i] << 8 | bytes[i + 1];
+                char c = (char)val;
+
+                if (char.IsSurrogate(c))
+                {
+                    DecoderFallback.CreateFallbackBuffer().Fallback(
+                        bytes.Slice(i, 2).ToArray(),
+                        i);
+
+                    Debug.Fail("Fallback should have thrown");
+                    throw new CryptographicException();
+                }
+
+                if (write)
+                {
+                    chars[writeIdx] = c;
+                }
+
+                writeIdx++;
+            }
+
+            return writeIdx;
+        }
+
+        public override int GetMaxByteCount(int charCount)
+        {
+            checked
+            {
+                return charCount * 2;
+            }
+        }
+
+        public override int GetMaxCharCount(int byteCount)
+        {
+            return byteCount / 2;
         }
     }
 }
