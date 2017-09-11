edd6ad6a4d01676f482fbf8d4196ec76c7b0863b
Ian Hays <ianha@microsoft.com>
2017-09-11 15:25:14 -0700

    Fix up the ref files to be correct for all TargetGroups

--- src/Common/src/System/Security/Cryptography/ECDiffieHellmanCng.DeriveFrom.cs
+++ src/Common/src/System/Security/Cryptography/ECDiffieHellmanCng.DeriveFrom.cs
@@ -15,44 +15,6 @@ namespace System.Security.Cryptography
 #endif
         public sealed partial class ECDiffieHellmanCng : ECDiffieHellman
         {
-            /// <summary>
-            /// Given a second party's public key, derive shared key material
-            /// </summary>
-            public override byte[] DeriveKeyMaterial(ECDiffieHellmanPublicKey otherPartyPublicKey)
-            {
-                Contract.Ensures(Contract.Result<byte[]>() != null);
-
-                if (otherPartyPublicKey == null)
-                {
-                    throw new ArgumentNullException("otherPartyPublicKey");
-                }
-
-                ECDiffieHellmanCngPublicKey otherKey = otherPartyPublicKey as ECDiffieHellmanCngPublicKey;
-                if (otherKey == null)
-                {
-                    // We may be able to create a CngPublicKey from the otherPartyPublicKey. Note that this is a change from previous
-                    // netfx behavior where a failed cast results in a CryptoException immediately. This is to account for when the 
-                    // otherPartyPublicKey was created by ECDiffieHellman.Create().PublicKey in which case the type is the internal
-                    // Algorithms ECDiffieHellmanCngPublicKey implementation.
-                    ECParameters otherPartyParameters = otherPartyPublicKey.ExportParameters();
-                    using (ECDiffieHellmanCng otherPartyCng = new ECDiffieHellmanCng())
-                    {
-                        try
-                        {
-                            // If the otherkey doesn't represent an object that can be loaded as a CNG key then ImportParameters throws a CryptoException.
-                            otherPartyCng.ImportParameters(otherPartyParameters);
-                            otherKey = otherPartyCng.PublicKey as ECDiffieHellmanCngPublicKey;
-                        }
-                        catch (CryptographicException)
-                        {
-                            throw new ArgumentException(SR.Cryptography_ArgExpectedECDiffieHellmanCngPublicKey);
-                        }
-                    }
-                }
-
-                return DeriveKeyMaterialFromCngKey(otherKey);
-            }
-
             public override byte[] DeriveKeyFromHash(
                 ECDiffieHellmanPublicKey otherPartyPublicKey,
                 HashAlgorithmName hashAlgorithm,
