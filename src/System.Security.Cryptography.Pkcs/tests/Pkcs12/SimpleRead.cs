8d07469ac1af21510e72a6ee710e87911144108b
Jeremy Barton <jbarton@microsoft.com>
2018-06-21 14:53:25 -0700

    Copy tests from other branch

---
+++ src/System.Security.Cryptography.Pkcs/tests/Pkcs12/SimpleRead.cs
@@ -0,0 +1,149 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using Test.Cryptography;
+using Xunit;
+
+namespace System.Security.Cryptography.Pkcs.Tests.Pkcs12
+{
+    public static class SimpleRead
+    {
+        [Fact]
+        public static void Test1()
+        {
+            var loader = (CertLoaderFromRawData)Certificates.RSAKeyTransferCapi1;
+            ReadOnlyMemory<byte> pfxData = loader.PfxData;
+
+            Pkcs12Info info = Pkcs12Info.Decode(pfxData, out int bytesConsumed);
+            Assert.Equal(pfxData.Length, bytesConsumed);
+
+            Assert.Equal(Pkcs12Info.IntegrityMode.Password, info.DataIntegrityMode);
+            CheckMac(info, loader.Password);
+
+            ReadOnlyCollection<Pkcs12SafeContents> authSafe = info.AuthenticatedSafe;
+            Assert.Same(authSafe, info.AuthenticatedSafe);
+            Assert.Equal(2, authSafe.Count);
+
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.None, authSafe[0].DataConfidentialityMode);
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.None, authSafe[1].DataConfidentialityMode);
+
+            List<Pkcs12SafeBag> safe0Bags = new List<Pkcs12SafeBag>(authSafe[0].GetBags());
+
+            Assert.Equal(1, safe0Bags.Count);
+            ShroudedKeyBag shroudedKeyBag = Assert.IsType<ShroudedKeyBag>(safe0Bags[0]);
+
+            List<Pkcs12SafeBag> safe1Bags = new List<Pkcs12SafeBag>(authSafe[1].GetBags());
+
+            Assert.Equal(1, safe0Bags.Count);
+            Assert.IsType<CertBag>(safe1Bags[0]);
+            CertBag certBag = (CertBag)safe1Bags[0];
+
+            Assert.True(certBag.IsX509Certificate, "certBag.IsX509Certificate");
+            Assert.InRange(certBag.RawData.Length, loader.CerData.Length + 2, int.MaxValue);
+
+            byte[] data = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            byte[] encrypted;
+
+            using (X509Certificate2 fromLoader = loader.GetCertificate())
+            using (X509Certificate2 fromBag = certBag.GetCertificate())
+            using (RSA loaderPub = fromLoader.GetRSAPublicKey())
+            {
+                Assert.Equal(fromLoader.RawData, fromBag.RawData);
+
+                encrypted = loaderPub.Encrypt(data, RSAEncryptionPadding.OaepSHA1);
+            }
+
+            int bytesRead;
+
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportEncryptedPkcs8PrivateKey(
+                    loader.Password,
+                    shroudedKeyBag.EncryptedPkcs8PrivateKey.Span,
+                    out bytesRead);
+
+                byte[] dec = rsa.Decrypt(encrypted, RSAEncryptionPadding.OaepSHA1);
+                Assert.Equal(data, dec);
+            }
+
+            Assert.Equal(shroudedKeyBag.EncryptedPkcs8PrivateKey.Length, bytesRead);
+        }
+
+        [Fact]
+        public static void ReadWithEncryptedContents()
+        {
+            var loader = (CertLoaderFromRawData)Certificates.RSAKeyTransfer_ExplicitSki;
+            ReadOnlyMemory<byte> pfxData = loader.PfxData;
+
+            Pkcs12Info info = Pkcs12Info.Decode(pfxData, out int bytesConsumed);
+            Assert.Equal(pfxData.Length, bytesConsumed);
+
+            Assert.Equal(Pkcs12Info.IntegrityMode.Password, info.DataIntegrityMode);
+            CheckMac(info, loader.Password);
+
+            ReadOnlyCollection<Pkcs12SafeContents> authSafe = info.AuthenticatedSafe;
+            Assert.Same(authSafe, info.AuthenticatedSafe);
+            Assert.Equal(2, authSafe.Count);
+
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.Password, authSafe[0].DataConfidentialityMode);
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.None, authSafe[1].DataConfidentialityMode);
+
+            Assert.ThrowsAny<CryptographicException>(
+                () => authSafe[0].Decrypt(loader.Password.AsSpan().Slice(1)));
+
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.Password, authSafe[0].DataConfidentialityMode);
+            authSafe[0].Decrypt(loader.Password);
+            Assert.Equal(Pkcs12SafeContents.ConfidentialityMode.None, authSafe[0].DataConfidentialityMode);
+
+            List<Pkcs12SafeBag> safe0Bags = new List<Pkcs12SafeBag>(authSafe[0].GetBags());
+            Assert.Equal(1, safe0Bags.Count);
+            CertBag certBag = Assert.IsType<CertBag>(safe0Bags[0]);
+
+            Assert.True(certBag.IsX509Certificate, "certBag.IsX509Certificate");
+            Assert.InRange(certBag.RawData.Length, loader.CerData.Length + 2, int.MaxValue);
+
+            List<Pkcs12SafeBag> safe1Bags = new List<Pkcs12SafeBag>(authSafe[1].GetBags());
+            Assert.Equal(1, safe0Bags.Count);
+            ShroudedKeyBag shroudedKeyBag = Assert.IsType<ShroudedKeyBag>(safe1Bags[0]);
+
+            byte[] data = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            byte[] encrypted;
+
+            using (X509Certificate2 fromLoader = loader.GetCertificate())
+            using (X509Certificate2 fromBag = certBag.GetCertificate())
+            using (RSA loaderPub = fromLoader.GetRSAPublicKey())
+            {
+                Assert.Equal(fromLoader.RawData, fromBag.RawData);
+
+                encrypted = loaderPub.Encrypt(data, RSAEncryptionPadding.OaepSHA1);
+            }
+
+            int bytesRead;
+
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportEncryptedPkcs8PrivateKey(
+                    loader.Password,
+                    shroudedKeyBag.EncryptedPkcs8PrivateKey.Span,
+                    out bytesRead);
+
+                byte[] dec = rsa.Decrypt(encrypted, RSAEncryptionPadding.OaepSHA1);
+                Assert.Equal(data, dec);
+            }
+
+            Assert.Equal(shroudedKeyBag.EncryptedPkcs8PrivateKey.Length, bytesRead);
+        }
+
+        private static void CheckMac(Pkcs12Info info, string password)
+        {
+            Assert.True(info.VerifyMac(password), "VerifyMac (correct password)");
+            Assert.False(info.VerifyMac(ReadOnlySpan<char>.Empty), "VerifyMac (empty password)");
+            Assert.False(info.VerifyMac(password + password), "VerifyMac (doubled password)");
+            Assert.False(info.VerifyMac(new string('a', 1048)), "VerifyMac (password > 1k)");
+        }
+    }
+}
---
+++ src/System.Security.Cryptography.Pkcs/tests/Pkcs12/WriteToWindows.cs
@@ -0,0 +1,254 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.Cryptography.X509Certificates;
+using Test.Cryptography;
+using Xunit;
+
+namespace System.Security.Cryptography.Pkcs.Tests.Pkcs12
+{
+    [PlatformSpecific(TestPlatforms.Windows)]
+    public static class WriteToWindows
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(999)]
+        [InlineData(1000)]
+        [InlineData(1024)]
+        [InlineData(2048)]
+        [InlineData(10000)]
+        [InlineData(123321)]
+        public static void WriteEmpty(int iterationCount)
+        {
+            Pkcs12Builder builder = new Pkcs12Builder();
+            string password = $"Password{iterationCount}IsMyVoice";
+
+            // Windows 7 through 10-1709 only support SHA-1 as the MAC PRF
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, iterationCount);
+
+            byte[] emptyPfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(emptyPfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(0, coll.Collection.Count);
+            }
+        }
+
+        [Fact]
+        public static void WriteOneCertNoKeys_NoEncryption()
+        {
+            Pkcs12SafeContents contents = new Pkcs12SafeContents();
+            byte[] rawData;
+
+            using (X509Certificate2 cert = Certificates.RSAKeyTransferCapi1.GetCertificate())
+            {
+                contents.AddCertificate(cert);
+                rawData = cert.RawData;
+            }
+
+            Pkcs12Builder builder = new Pkcs12Builder();
+            builder.AddSafeContentsUnencrypted(contents);
+
+            const string password = nameof(WriteOneCertNoKeys_NoEncryption);
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, 1024);
+            byte[] pfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(pfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(1, coll.Collection.Count);
+                Assert.Equal(rawData, coll.Collection[0].RawData);
+                Assert.False(coll.Collection[0].HasPrivateKey, "coll.Collection[0].HasPrivateKey");
+            }
+        }
+
+        [Fact]
+        public static void WriteOneCertNoKeys_Encrypted()
+        {
+            Pkcs12SafeContents contents = new Pkcs12SafeContents();
+            byte[] rawData;
+
+            using (X509Certificate2 cert = Certificates.RSAKeyTransferCapi1.GetCertificate())
+            {
+                contents.AddCertificate(cert);
+                rawData = cert.RawData;
+            }
+
+            const string password = nameof(WriteOneCertNoKeys_NoEncryption);
+
+            Pkcs12Builder builder = new Pkcs12Builder();
+            builder.AddSafeContentsEncrypted(
+                contents,
+                password,
+                new PbeParameters(
+                    PbeEncryptionAlgorithm.TripleDes3KeyPkcs12,
+                    HashAlgorithmName.SHA1,
+                    2050));
+
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, 1024);
+            byte[] pfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(pfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(1, coll.Collection.Count);
+                Assert.Equal(rawData, coll.Collection[0].RawData);
+                Assert.False(coll.Collection[0].HasPrivateKey, "coll.Collection[0].HasPrivateKey");
+            }
+        }
+
+        [Fact]
+        public static void WriteOneCertWithKey_Encrypted_SameSafe()
+        {
+            Pkcs12SafeContents contents = new Pkcs12SafeContents();
+            byte[] rawData;
+
+            AsnEncodedData localKeyId = new AsnEncodedData(
+                "1.2.840.113549.1.9.21",
+                "040101".HexToByteArray());
+
+            using (X509Certificate2 cert = Certificates.RSAKeyTransferCapi1.TryGetCertificateWithPrivateKey())
+            using (RSA certKey = cert.GetRSAPrivateKey())
+            using (RSA exportableKey = certKey.MakeExportable())
+            {
+                CertBag certBag = contents.AddCertificate(cert);
+                certBag.Attributes.Add(localKeyId);
+
+                rawData = cert.RawData;
+
+                KeyBag keyBag = contents.AddKeyUnencrypted(exportableKey.ExportPkcs8PrivateKey());
+
+                keyBag.Attributes.Add(localKeyId);
+            }
+
+            const string password = nameof(WriteOneCertWithKey_Encrypted_SameSafe);
+
+            Pkcs12Builder builder = new Pkcs12Builder();
+
+            builder.AddSafeContentsEncrypted(
+                contents,
+                password,
+                new PbeParameters(
+                    PbeEncryptionAlgorithm.TripleDes3KeyPkcs12,
+                    HashAlgorithmName.SHA1,
+                    2050));
+
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, 1024);
+            byte[] pfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(pfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(1, coll.Collection.Count);
+                Assert.Equal(rawData, coll.Collection[0].RawData);
+                Assert.True(coll.Collection[0].HasPrivateKey, "coll.Collection[0].HasPrivateKey");
+            }
+        }
+
+        [Fact]
+        public static void WriteOneCertWithKey_LikeWindows()
+        {
+            Pkcs12SafeContents safe1 = new Pkcs12SafeContents();
+            Pkcs12SafeContents safe2 = new Pkcs12SafeContents();
+            byte[] rawData;
+
+            AsnEncodedData localKeyId = new AsnEncodedData(
+                "1.2.840.113549.1.9.21",
+                "040101".HexToByteArray());
+
+            const string password = nameof(WriteOneCertWithKey_LikeWindows);
+
+            using (X509Certificate2 cert = Certificates.RSAKeyTransferCapi1.TryGetCertificateWithPrivateKey())
+            {
+                CertBag certBag = safe1.AddCertificate(cert);
+                certBag.Attributes.Add(localKeyId);
+
+                rawData = cert.RawData;
+
+                ShroudedKeyBag keyBag = safe2.AddShroudedKey(
+                    cert.GetRSAPrivateKey().ExportEncryptedPkcs8PrivateKey(
+                        password,
+                        new PbeParameters(
+                            PbeEncryptionAlgorithm.TripleDes3KeyPkcs12,
+                            HashAlgorithmName.SHA1,
+                            2068)));
+
+                keyBag.Attributes.Add(localKeyId);
+            }
+
+            Pkcs12Builder builder = new Pkcs12Builder();
+
+            builder.AddSafeContentsEncrypted(
+                safe1,
+                password,
+                new PbeParameters(
+                    PbeEncryptionAlgorithm.TripleDes3KeyPkcs12,
+                    HashAlgorithmName.SHA1,
+                    2068));
+
+            builder.AddSafeContentsUnencrypted(safe2);
+
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, 2068);
+            byte[] pfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(pfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(1, coll.Collection.Count);
+                Assert.Equal(rawData, coll.Collection[0].RawData);
+                Assert.True(coll.Collection[0].HasPrivateKey, "coll.Collection[0].HasPrivateKey");
+            }
+        }
+
+        [Fact]
+        public static void WriteTwoCertsNoKeys_NoEncryption()
+        {
+            Pkcs12SafeContents contents = new Pkcs12SafeContents();
+            byte[] rawData1;
+            byte[] rawData2;
+
+            using (X509Certificate2 cert1 = Certificates.RSAKeyTransferCapi1.GetCertificate())
+            using (X509Certificate2 cert2 = Certificates.RSAKeyTransfer2.GetCertificate())
+            {
+                // Windows seems to treat these as a stack.  (LIFO)
+                contents.AddCertificate(cert2);
+                contents.AddCertificate(cert1);
+                rawData1 = cert1.RawData;
+                rawData2 = cert2.RawData;
+            }
+
+            Pkcs12Builder builder = new Pkcs12Builder();
+            builder.AddSafeContentsUnencrypted(contents);
+
+            const string password = nameof(WriteOneCertNoKeys_NoEncryption);
+            builder.SealAndMac(password, HashAlgorithmName.SHA1, 1024);
+            byte[] pfx = builder.Encode();
+
+            ImportedCollection coll =
+                ImportedCollection.Import(pfx, password, X509KeyStorageFlags.EphemeralKeySet);
+
+            using (coll)
+            {
+                Assert.Equal(2, coll.Collection.Count);
+                Assert.Equal(rawData1, coll.Collection[0].RawData);
+                Assert.Equal(rawData2, coll.Collection[1].RawData);
+                Assert.False(coll.Collection[0].HasPrivateKey, "coll.Collection[0].HasPrivateKey");
+                Assert.False(coll.Collection[1].HasPrivateKey, "coll.Collection[1].HasPrivateKey");
+            }
+        }
+    }
+}
