be48cee360115b4f443ae2f7725f6dc1702b7a52
Jeremy Barton <jbarton@microsoft.com>
2018-06-26 07:09:13 -0700

    Dead code

--- src/System.Security.Cryptography.Pkcs/tests/Pkcs12/CertBagTests.cs
+++ src/System.Security.Cryptography.Pkcs/tests/Pkcs12/CertBagTests.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Security.Cryptography.X509Certificates;
-using Test.Cryptography;
 using Xunit;
 
 namespace System.Security.Cryptography.Pkcs.Tests.Pkcs12
