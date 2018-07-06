4d2d4c4f389f0eb55052c6a5ad85262427487004
Jeremy Barton <jbarton@microsoft.com>
2018-07-06 09:55:32 -0700

    KeyBag tests

--- src/System.Security.Cryptography.Pkcs/src/System/Security/Cryptography/Pkcs/KeyBag.cs
+++ src/System.Security.Cryptography.Pkcs/src/System/Security/Cryptography/Pkcs/KeyBag.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Security.Cryptography.Asn1;
+
 namespace System.Security.Cryptography.Pkcs
 {
     public sealed class KeyBag : Pkcs12SafeBag
@@ -11,6 +13,11 @@ namespace System.Security.Cryptography.Pkcs
         public KeyBag(ReadOnlyMemory<byte> pkcs8PrivateKey, bool skipCopy=false)
             : base(Oids.Pkcs12KeyBag)
         {
+            // Read to ensure that there is precisely one legally encoded value.
+            AsnReader reader = new AsnReader(pkcs8PrivateKey, AsnEncodingRules.BER);
+            reader.GetEncodedValue();
+            reader.ThrowIfNotEmpty();
+
             Pkcs8PrivateKey = skipCopy ? pkcs8PrivateKey : pkcs8PrivateKey.ToArray();
         }
 
